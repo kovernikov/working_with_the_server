68e118ab49f6678529ecb74a19a00e933647fb7a
Kovernikov_A <kovernikov@post.cz>
2021-04-17 16:50:00 +0200

    sacond commit

--- js/script.js
+++ js/script.js
@@ -1,44 +1,70 @@
 window.addEventListener('DOMContentLoaded', ()=> {
     function req() {
-        const reqvest = new XMLHttpRequest();
-        reqvest.open('GET', 'http://localhost:3000/people');
-        reqvest.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        reqvest.send();
-        // reqvest.addEventListener('readystatechange', function() {
-        //     if(reqvest.readyState === 4 && reqvest.status == 200) {
-            reqvest.addEventListener('load', function() {
-            if(reqvest.status == 200) {
-                let data = JSON.parse(reqvest.response);
-                // console.log(data);
+        // const reqvest = new XMLHttpRequest();
+        // reqvest.open('GET', 'http://localhost:3000/people');
+        // reqvest.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        // reqvest.send();
+        // // reqvest.addEventListener('readystatechange', function() {
+        // //     if(reqvest.readyState === 4 && reqvest.status == 200) {
+        //     reqvest.addEventListener('load', function() {
+        //     if(reqvest.status == 200) {
+        //         let data = JSON.parse(reqvest.response);
+        //         // console.log(data);
+        //         createCard(data);
 
-                data.forEach(item => {
-                    let card = document.createElement('div');
+                
+        //     } else {
+        //         console.error('Что-то пошло не так');
+        //     }
+        // });
+        getResourse("http://localhost:3000/people")
+            .then(data => createCard(data.data))
+            .catch(err => console.error(err));
 
-                    card.classList.add('card');
-
-                    let icon;
-                    if(item.sex === 'male') {
-                        icon = "icons/mars.png"
-                    } else {
-                        icon = "icons/female.png"
-                    }
-
-                    card.innerHTML = `
-                        <img src="${item.photo}" alt="photo">
-                        <div class="name">${item.name} ${item.surname}</div>
-                        <div class="sex">
-                            <img src=${icon} alt="male">
-                        </div>
-                        <div class="age">${item.age}</div>
-                    `;
-                    document.querySelector('.app').appendChild(card);
-                });
-            } else {
-                console.error('Что-то пошло не так');
-            }
-        });
-        console.log(this);
-        this.remove();
+        this.style.display = 'none';
     }
     document.querySelector('button').addEventListener('click', req, {'once': true});
+
+    // async function getResourse(url) {
+    //     const res = await fetch(`${url}`);
+
+    //     if (!res.ok) {
+    //         throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    //     }
+    //     return await res.json();
+    // }
+
+    async function getResourse(url) {
+        const res = await axios(`${url}`);
+
+        if (res.status !== 200) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+        return res;
+    }
+
+    function createCard(response) {
+        response.forEach(item => {
+            let card = document.createElement('div');
+
+            card.classList.add('card');
+
+            let icon;
+            if(item.sex === 'male') {
+                icon = "icons/mars.png"
+            } else {
+                icon = "icons/female.png"
+            }
+
+            card.innerHTML = `
+                <img src="${item.photo}" alt="photo">
+                <div class="name">${item.name} ${item.surname}</div>
+                <div class="sex">
+                    <img src=${icon} alt="male">
+                </div>
+                <div class="age">${item.age}</div>
+            `;
+            document.querySelector('.app').appendChild(card);
+        });
+    }
 });
